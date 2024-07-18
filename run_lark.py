4e34928f0542b481fefe8a95ab7b3839e3df3056
tom-huntington <55266932+tom-huntington@users.noreply.github.com>
2024-07-18 14:02:02 +1200

    need lexer priority. Also require whitespace

--- run_lark.py
+++ run_lark.py
@@ -1,38 +1,36 @@
 from lark import Lark, logger
 from lark.indenter import Indenter
 import tokens
+import logging
 
 dyadic_quicks = ('chunk_fold', 'slide_fold')
 hofs1 = ' | '.join(f'"{k}"' for k in tokens.quick.keys() if k not in dyadic_quicks)
 hofs2 = ' | '.join(f'"{k}"' for k in dyadic_quicks)
-monads = ' | '.join(f'"{a}"' for a in [*tokens.dyadic.keys(), *tokens.monadic.keys()])
+# monads = ' | '.join(f'"{a} "' for a in [*tokens.dyadic.keys(), *tokens.monadic.keys()])
 
 
 grammar=f"""
 ?start: func_end 
-monad: monad3? func? (func_end | func) | monad "." func? (func_end | func)
-monad3: (monad "." | monad3 | func) func (func_end | func) -> monad
-dyad: dyad3? func? (func_end | func) | dyad "." func? (func_end | func)
-dyad3: (dyad ":" | dyad3 | func) func (func_end | func) -> dyad
-?func_end: "\\\\" monad | "|" dyad 
-?func: "(" monad ")" | "{{" dyad "}}" | builtin | hof | literal
-!hof: (HOFS1 func) | (HOFS2 func func)
+monad: (monad3 _ws)? (func _ws)? (func_end | func) | monad "." (func _ws)? (func_end | func)
+monad3: (monad _ws "." | monad3 | func) _ws func _ws (func_end | func) -> monad
+dyad: (dyad3 _ws)? (func _ws)? (func_end | func) | dyad "." (func _ws)? (func_end | func)
+dyad3: (dyad _ws ":" | dyad3 | func) _ws func _ws (func_end | func) -> dyad
+?func_end: "\\\\" _ws monad | "|" _ws dyad
+?func: "(" monad ")" | "{{" dyad "}}" | hof | builtin | literal
+!hof: (HOFS1 _ws func) | (HOFS2 _ws func _ws func)
 HOFS1: {hofs1}
-HOFS2: {hofs2}
+HOFS2 . 1: {hofs2}
 builtin: BUILTIN_DYAD | BUILTIN_MONAD
 BUILTIN_DYAD: {' | '.join(f'"{a}"' for a in tokens.dyadic.keys())}
-BUILTIN_MONAD: {' | '.join(f'"{a}"' for a in tokens.monadic.keys())}
-dot: "."
+BUILTIN_MONAD: {' | '.join(f'"{a}"' for a in tokens.monadic.keys() if a != "part")}
 
 number: SIGNED_NUMBER
 literal: number
 %import common.SIGNED_NUMBER
+%import common.WS
+%ignore WS
 
-%import common.CNAME -> NAME
-%import common.WS_INLINE
-%import common.NEWLINE
-%ignore WS_INLINE
-%ignore NEWLINE
+_ws: (" " | "\\n" | "\\t")+
 """
 
 # literal: string
@@ -54,16 +52,16 @@ literal: number
 print(grammar)
 
 sample_string ="""\
-\ pair 1
-"""
+| chunk_fold + 2"""
 # sample_string="\ scan pair"
 # logger.setLevel(logging.DEBUG)
 if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
     print(sample_string)
-    parser = Lark(grammar, debug=False)
+    parser = Lark(grammar, debug=False, strict=True)
     for i, t in enumerate(parser.lex(sample_string)):
         print((t.line, t.column), repr(t))
     parse_tree = parser.parse(sample_string)
-    print(parse_tree)
     print(parse_tree.pretty())
+    print(parse_tree)
 
